1dd9d49b16534811369c93b8374f36b6ff28e130
Nathaniel Shirts <nshirts7@gmail.com>
2021-02-18 21:49:59 -0700

    started working on the js

---
+++ script.js
@@ -0,0 +1,38 @@
+const theNums = document.querySelectorAll('[data-number]');
+const theOps = document.querySelectorAll('[data-ops]')
+const theAc = document.querySelectorAll('[data-ac]')
+const theDel = document.querySelectorAll('[data-del]')
+const theEqual = document.querySelectorAll('[data-equal]')
+
+theNums.forEach(val => {
+    val.addEventListener('click', () => console.log(val))
+})
+
+theOps.forEach(val => {
+    val.addEventListener('click', () => console.log(val))
+})
+
+theAc.forEach(val => {
+    val.addEventListener('click', () => console.log(val))
+})
+
+theDel.forEach(val => {
+    val.addEventListener('click', () => console.log(val))
+})
+
+theEqual.forEach(val => {
+    val.addEventListener('click', () => console.log(val))
+})
+
+const calculator = {
+    currentNumber: 0,
+    
+    operation() {},
+    delete() {},
+    display() {
+        let bottomOutput = document.querySelector('.bottom-output');
+        console.log(bottomOutput)
+        bottomOutput.innerText = this.currentNumber;
+    },
+};
+calculator.display()
